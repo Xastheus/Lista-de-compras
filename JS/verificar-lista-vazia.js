7759de2d21a631c9f69969b90f61a5932dc90b79
Matheus Barboza <matheusbarbozagarcia2001@gmail.com>
2025-01-13 17:19:15 -0300

    Eu consertei o probleminha que estava acontecendo que fazia a mensagem e a imagem aparecer quando a gente mandava um item de volta para a lista de adicionados quando a recarregávamos a página pela primeira vez. Agora está show!

--- JS/verificar-lista-vazia.js
+++ JS/verificar-lista-vazia.js
@@ -2,12 +2,11 @@ const mensagemListaVazia = document.querySelector(".mensagem-vazia");
 const imagemDog = document.querySelector(".dog");
 
 export function verificarListaVazia(lista) {
-    if (lista.querySelectorAll("li").length === 0) {
-        mensagemListaVazia.style.display = "block";  // Aparece apenas quando a lista está vazia
-        imagemDog.style.display = "block";      // Aparece apenas quando a lista está vazia
-    } else {
-        mensagemListaVazia.style.display = "none";
-        imagemDog.style.display = "none";
-    }
-
+  if (lista.querySelectorAll("li").length === 0) {
+    mensagemListaVazia.style.display = "block"; // Aparece apenas quando a lista está vazia
+    imagemDog.style.display = "block"; // Aparece apenas quando a lista está vazia
+  } else {
+    mensagemListaVazia.style.display = "none";
+    imagemDog.style.display = "none";
+  }
 }
